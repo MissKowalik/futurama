f19c0020fc4538316f1f31ad53aae561933b076b
Linda <lindakowalik@gmail.com>
2025-08-20 16:10:33 +0200

    fetch characters via graphql api

--- app/graphql/page.tsx
+++ app/graphql/page.tsx
@@ -1,5 +1,19 @@
-export default function Rest() {
+import CharacterCard from "@/components/CharacterCard";
+import { getCharactersGraphQL } from "@/lib/data/graphql";
+
+export default async function GraphQL() {
+    const characters = await getCharactersGraphQL();
+
     return (
-        <h2>GraphQL</h2>
-    )
+        <>
+            <h2 className="text-zinc-200 text-2xl m-4">Fetched with GraphQL</h2>
+
+            {/* Character Card */}
+            <div className="grid grid-cols-4 gap-4 mx-4">
+                {characters.map(character => (
+                    <CharacterCard key={character.id} character={character} />
+                ))}
+            </div>
+        </>
+    );
 }
---
+++ lib/data/graphql.ts
@@ -0,0 +1,32 @@
+import { Character } from "../interfaces/Character";
+
+const endpoint = "https://futuramaapi.com/graphql"
+
+const query = `
+{
+  characters {
+    edges {
+      id
+      name
+      gender
+      status
+      species
+      image
+    }
+  }
+}
+`;
+
+export async function getCharactersGraphQL() {
+  const response = await fetch(endpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ query: query }),
+  });
+
+  const result = await response.json();
+  const characters: Character[] = result.data.characters.edges;
+  return characters;
+}
